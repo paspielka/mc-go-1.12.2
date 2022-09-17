b07f11de79c0b190ab0e261e029a0b8e68ab095f
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-09-17 12:05:15 -0400

    Feature: Vector Maths

--- struct/Vector2.go
+++ struct/Vector2.go
@@ -4,3 +4,35 @@ package _struct
 type Vector2 struct {
 	X, Y float64
 }
+
+func (v2 Vector2) Add(v Vector2) Vector2 {
+	return Vector2{v2.X + v.X, v2.Y + v.Y}
+}
+
+func (v2 Vector2) Sub(v Vector2) Vector2 {
+	return Vector2{v2.X - v.X, v2.Y - v.Y}
+}
+
+func (v2 Vector2) Mul(v Vector2) Vector2 {
+	return Vector2{v2.X * v.X, v2.Y * v.Y}
+}
+
+func (v2 Vector2) Div(v Vector2) Vector2 {
+	return Vector2{v2.X / v.X, v2.Y / v.Y}
+}
+
+func (v2 Vector2) DistanceTo(v Vector2) float64 {
+	return v2.Sub(v).Length()
+}
+
+func (v2 Vector2) Length() float64 {
+	return (v2.X * v2.X) + (v2.Y * v2.Y)
+}
+
+func (v2 Vector2) LengthSquared() float64 {
+	return v2.Length() * v2.Length()
+}
+
+func (v2 Vector2) Normalize() Vector2 {
+	return v2.Div(Vector2{v2.Length(), v2.Length()})
+}
--- struct/Vector3.go
+++ struct/Vector3.go
@@ -13,6 +13,34 @@ func (v3 Vector3) Add(v Vector3) Vector3 {
 	return Vector3{v3.X + v.X, v3.Y + v.Y, v3.Z + v.Z}
 }
 
+func (v3 Vector3) Sub(v Vector3) Vector3 {
+	return Vector3{v3.X - v.X, v3.Y - v.Y, v3.Z - v.Z}
+}
+
+func (v3 Vector3) Mul(v Vector3) Vector3 {
+	return Vector3{v3.X * v.X, v3.Y * v.Y, v3.Z * v.Z}
+}
+
+func (v3 Vector3) Div(v Vector3) Vector3 {
+	return Vector3{v3.X / v.X, v3.Y / v.Y, v3.Z / v.Z}
+}
+
+func (v3 Vector3) DistanceTo(v Vector3) float64 {
+	return v3.Sub(v).Length()
+}
+
+func (v3 Vector3) Length() float64 {
+	return (v3.X * v3.X) + (v3.Y * v3.Y) + (v3.Z * v3.Z)
+}
+
+func (v3 Vector3) LengthSquared() float64 {
+	return v3.Length() * v3.Length()
+}
+
+func (v3 Vector3) Normalize() Vector3 {
+	return v3.Div(Vector3{v3.Length(), v3.Length(), v3.Length()})
+}
+
 func PackVector3(v3 Vector3) (p []byte) {
 	var data []byte
 	data = append(data, PackDouble(v3.X)...)
