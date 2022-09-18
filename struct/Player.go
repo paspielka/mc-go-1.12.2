807fc0bc11cbeeeb02e4633ee478e69580fb4d92
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-09-17 23:22:26 -0400

    wip: Inventory

--- struct/Player.go
+++ struct/Player.go
@@ -24,3 +24,20 @@ func (p *Player) GetPosition() Vector3 {
 func (p *Player) GetBlockPos() (x, y, z int) {
 	return int(math.Floor(p.X)), int(math.Floor(p.Y)), int(math.Floor(p.Z))
 }
+
+func (p *Player) GetItemSlotByID(id int) Slot {
+	for _, item := range p.Inventory {
+		if item.ID == id {
+			return item
+		}
+	}
+	return Slot{}
+}
+
+func (p *Player) GetItemSlot(slot int16) Slot {
+	return p.Inventory[slot]
+}
+
+func (p *Player) GetItemAt(slot int16) Slot {
+	return p.Inventory[slot]
+}
