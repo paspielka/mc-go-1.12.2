c0b635b76a274b143d5c6b51346ace87eb6dfd46
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2022-09-23 22:32:58 -0400

    Fix: Doesn't join even with fallback

--- struct/Auth.go
+++ struct/Auth.go
@@ -95,16 +95,11 @@ func (p *Auth) JoinServer(addr string, port int) (g *Game, err error) {
 	}
 	// Fallback If SRV record not found
 	if ras == "" {
-		e := p.PingServer(addr, port)
-		if e == nil {
-			ras = fmt.Sprintf("%s:%d", addr, port)
-		} else {
-			err = fmt.Errorf("cannot connect the server %q: %v", addr, e)
-			return
-		}
+		ras = fmt.Sprintf("%s:%d", addr, port)
 	}
 
 	// Connection
+	fmt.Println("Connecting to", ras)
 	g = new(Game)
 	g.Conn, err = net.Dial("tcp", ras)
 	if err != nil {
